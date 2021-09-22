5a0be9606037d4987e06f67146cd89888d81b3f9
unknown <awsdev747@gmail.com>
2021-09-22 10:18:31 +0530

    hibenate demo

--- Hibernate/src/com/hibernate/ManageEmployee.java
+++ Hibernate/src/com/hibernate/ManageEmployee.java
@@ -5,44 +5,69 @@ package com.hibernate;
 import org.hibernate.HibernateException; 
 import org.hibernate.Session; 
 import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class ManageEmployee {
-   private static SessionFactory factory; 
+
+	
+	   static SessionFactory sessionFactoryObj;
+	   static Session sessionObj;
+	 
+	    private static SessionFactory buildSessionFactory() {
+	        // Creating Configuration Instance & Passing Hibernate Configuration File
+	        Configuration configObj = new Configuration();
+	        configObj.configure("hibernate.cfg.xml");
+	 
+	        // Since Hibernate Version 4.x, ServiceRegistry Is Being Used
+	        ServiceRegistry serviceRegistryObj = new StandardServiceRegistryBuilder().applySettings(configObj.getProperties()).build(); 
+	 
+	        // Creating Hibernate SessionFactory Instance
+	        sessionFactoryObj = configObj.buildSessionFactory(serviceRegistryObj);
+	        return sessionFactoryObj;
+	    }
+	
+	
+	
    public static void main(String[] args) {
-      
+
       try {
-         factory = new Configuration().configure().buildSessionFactory();
-      } catch (Throwable ex) { 
-         System.err.println("Failed to create sessionFactory object." + ex);
-         throw new ExceptionInInitializerError(ex); 
+    	   sessionObj = buildSessionFactory().openSession();
+           sessionObj.beginTransaction();
+
+      } catch (Exception me) { 
+         System.err.println("Failed to create sessionFactory object." + me);
+         
       }
       
       ManageEmployee ME = new ManageEmployee();
 
       /* Add few employee records in database */
-      Integer empID1 = ME.addEmployee("ram", "kumar", 1000);
+      Integer empID1 = ME.addEmployee("anshul", "kumar", 1000);//asssinged values from here
+      Integer empID2 = ME.addEmployee("ankit1", "kumar", 50000);//asssinged values from here
 
 
    }
    
-   /* Method to CREATE an employee in the database */
+   /* Method to CREATE an employee in the database   business logic*/
    public Integer addEmployee(String fname, String lname, int salary){
-      Session session = factory.openSession();
+     sessionObj = sessionFactoryObj.openSession();
       Transaction tx = null;
       Integer employeeID = null;
       
       try {
-         tx = session.beginTransaction();
-         Employee employee = new Employee(fname, lname, salary);
-         employeeID = (Integer) session.save(employee); 
-         tx.commit();
+         tx = sessionObj.beginTransaction();
+         Employee employee = new Employee(fname, lname, salary);//created the object of employee class
+         employeeID = (Integer) sessionObj.save(employee); //save the object or insert the recording
+       
+         tx.commit();//explictiy call  the commit esure that auto commite should be false
       } catch (HibernateException e) {
          if (tx!=null) tx.rollback();
          e.printStackTrace(); 
       } finally {
-         session.close(); 
+    	  sessionObj.close(); 
       }
       return employeeID;
    }
