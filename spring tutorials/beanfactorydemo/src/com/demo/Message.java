7c65f160f10faad3ee008a5344efba7175559abe
unknown <awsdev747@gmail.com>
2021-09-27 15:42:14 +0530

    spring demo

---
+++ spring tutorials/beanfactorydemo/src/com/demo/Message.java
@@ -0,0 +1,15 @@
+package com.demo;
+
+public class Message {
+	
+	private String messsageinfo;//this call proerty
+
+	public String getMesssageinfo() {
+		return messsageinfo;
+	}
+
+	public void setMesssageinfo(String messsageinfo) {
+		this.messsageinfo = messsageinfo;
+	}
+
+}
