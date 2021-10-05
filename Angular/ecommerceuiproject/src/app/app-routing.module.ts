97ff4209b0feca4261f05323467b5c457bdd375b
unknown <awsdev747@gmail.com>
2021-10-05 15:54:13 +0530

    angular login  module

--- Angular/ecommerceuiproject/src/app/app-routing.module.ts
+++ Angular/ecommerceuiproject/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginmoduleComponent } from './loginmodule/loginmodule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginmoduleComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/ecommerceuiproject/src/app/app.module.ts
+++ Angular/ecommerceuiproject/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginmoduleComponent } from './loginmodule/loginmodule.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginmoduleComponent
   ],
   imports: [
     BrowserModule,
